33b2f45595fa8fff5effc3964dceb3b7fcfc5971
Tomasz Kumiega <kumiega.tomasz@gmail.com>
2017-11-20 23:38:02 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function checkOut() {
+    var liczba = document.getElementById('pole').value;
+    var btn = document.getElementById('btn');
+    
+    if (liczba > 0) {
+        document.getElementById('wynik').innerHTML='dodatnia';
+    } else if (liczba < 0) {
+        document.getElementById('wynik').innerHTML='ujemna';
+    } else if (liczba == 0) {
+        document.getElementById('wynik').innerHTML='zero';
+    } else {
+        document.getElementById('wynik').innerHTML='You know nothing Jon Snow!';
+    }
+}
+
+btn.addEventListener('click', function() {
+    checkOut();
+})
